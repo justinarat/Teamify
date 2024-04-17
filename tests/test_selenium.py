69bf6ea83f3e5a55b1f32db8af5dcf39a07afa6d
Dominic Davies <dominictdavies@gmail.com>
2024-04-18 02:00:39 +0800

    Add unittest to test_selenium.py

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,16 +1,18 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.remote.command import Command
 
-def test_route(driver, route):
-   response = driver.command_executor.execute(Command.GET, {"url": f"http://localhost:5000{route}"})
-   print(response)
+class SeleniumTestCase(unittest.TestCase):
+   def test_route(self, driver, route):
+      response = driver.command_executor.execute(Command.GET, {"url": f"http://localhost:5000{route}"})
+      print(response)
 
-   # if response == 0:
-   #    print(f"Accessed '{route}'")
-   # else:
-   #    print(f"Could not access '{route}'")
+      # if response == 0:
+      #    print(f"Accessed '{route}'")
+      # else:
+      #    print(f"Could not access '{route}'")
 
-   driver.quit()
+      driver.quit()
 
 if __name__ == "__main__":
-   test_route(webdriver.Chrome(), "/")
+    unittest.main(verbosity=2)

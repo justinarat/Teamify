abcb82f57dff62594df2a2998840595f2ae4cac9
Dominic Davies <dominictdavies@gmail.com>
2024-04-18 01:56:34 +0800

    Test 200 and 404 routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,11 +2,26 @@ import unittest
 import requests
 
 
-class ServerTestCase(unittest.TestCase):
-    def test_get_server(self):
-        server_url = "http://localhost:5000"
-        response = requests.get(server_url, timeout=10)
-        self.assertEqual(response.status_code, 200, f"Server responded with {response.status_code} rather than 200")
+class GetRoutesTestCase(unittest.TestCase):
+    def check_routes(self, routes, expected_status):
+        domain = "http://localhost:5000"
+        for route in routes:
+            with self.subTest(route=route):
+                url = domain + route
+                response = requests.get(url, timeout=10)
+                self.assertEqual(
+                    response.status_code,
+                    expected_status,
+                    f"Server responded with {response.status_code} rather than {expected_status} at '{url}'",
+                )
+
+    def test_get_ok_routes(self):
+        routes = ["/", "/test/route"]
+        self.check_routes(routes, 200)
+
+    def test_get_missing_routes(self):
+        routes = ["/page/not/found", "/this/does/not/exist"]
+        self.check_routes(routes, 404)
 
 
 if __name__ == "__main__":

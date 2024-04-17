1ffe618367299f65c024e78692607ae1ed188a5b
Dominic Davies <dominictdavies@gmail.com>
2024-04-17 18:00:13 +0800

    WIP reduce test scope

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,40 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.command import Command
 
-# create a new Firefox session
-driver = webdriver.Firefox()
-driver.maximize_window()
+def test_route(driver, route):
+   response = driver.command_executor.execute(Command.GET, {"url": f"http://localhost:5000{route}"})
+   print(response)
 
-# Navigate to the application home page
-driver.get("http://www.google.com")
+   # if response == 0:
+   #    print(f"Accessed '{route}'")
+   # else:
+   #    print(f"Could not access '{route}'")
 
-# get the search textbox
-search_field = driver.find_element(By.ID, "APjFqb")
+   driver.quit()
 
-search_field.clear()
-
-# enter search keyword and submit
-search_field.send_keys("Selenium WebDriver Interview questions")
-search_field.submit()
-
-# get the list of elements which are displayed after the search
-# currently on result page using find_elements_by_class_name method
-driver.implicitly_wait(5)
-lists = driver.find_element(By.ID, "rso")
-
-# get the number of elements found
-print ("Found " + str(len(lists)) + " searches:")
-
-# iterate through each element and print the text that is
-# name of the search
-
-i=0
-for listitem in lists:
-   print (listitem.get_attribute("innerHTML"))
-   i=i+1
-   if(i>10):
-      break
-
-# close the browser window
-driver.quit()
+if __name__ == "__main__":
+   test_route(webdriver.Chrome(), "/")

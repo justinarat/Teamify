1bd74a272586f4bf52f06e165601fb1485f05c6c
Dominic Davies <dominictdavies@gmail.com>
2024-04-17 20:05:33 +0800

    Format and can_get_url

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,27 +1,22 @@
 import requests
 
-def is_server_running(url: str):
+
+def can_get_url(url: str):
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return True
-    except requests.ConnectionError:
-        print(f"A connection error occured", end="")
-    except requests.Timeout:
-        print(f"Request timed out", end="")
-    except requests.TooManyRedirects:
-        print(f"Request exceeded the maximum number of redirections")
-        
-    print(f" when attempting to connect to '{url}'.")
-    return False
+        requests.get(url)
+        return True
+    except requests.RequestException as e:
+        return False
 
-def test_server():
-    url = "http://localhost:5000"
 
-    if is_server_running(url):
-        print(f"{url} is running.")
+def test_get_server():
+    server_url = "http://localhost:5000"
+
+    if can_get_url(server_url):
+        print(f"Got {server_url}")
     else:
-        print(f"{url} is not running.")
+        print(f"Could not get {server_url}")
+
 
 if __name__ == "__main__":
-    test_server()
+    test_get_server()

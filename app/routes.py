d70b660d215fa33ab368ebbee99a9680a75b96f1
Justin Arat <aratjustin@gmail.com>
2024-04-18 16:09:24 +0800

    Add endpoints for other pages
    Except for lobby-making if it's a pop up

--- app/routes.py
+++ app/routes.py
@@ -19,3 +19,21 @@ def introduction():
 @app.route("/games")
 def games_view():
   return render_template("games.html")
+
+@app.route("/lobby-searching")
+def lobby_searching():
+  return render_template("lobby-searching.html")
+
+@app.route("/lobby/<lobby_code>")
+def lobby_view(lobby_code):
+  # TODO: Render the right lobby using the lobby code
+  return render_template("lobby-view.html")
+
+@app.route("/account-creation")
+def account_creation():
+  return render_template("account-creation.html")
+
+@app.route("/admin")
+def admin():
+  # TODO: maybe use session information (or other way) to verify the user is an admin
+  return render_template("admin.html")

e431d87d30a080fa0aacb2d1a2388922e6acce68
JohnGiampaolo <giagio@LAPTOP-KA47AL5K>
2024-04-19 12:21:22 +0800

    Replaced toggle button with a yes/no dropdown functionality. If yes, show time settings

--- pages/pages.js
+++ pages/pages.js
@@ -1,9 +1,25 @@
-function showTime(day, event) {
-  event.preventDefault();
-  var element = document.getElementById(day);
-  if (element.style.display === "none") {
-    element.style.display = "block"; // or "inline" depending on your needs
-  } else {
-    element.style.display = "none";
+console.log("Script is running");
+document.getElementById("mon-toggle").addEventListener("change", showTime);
+
+function showTime(){
+  console.log("Change detected!");
+  var element = document.getElementById("monday");
+  var selectedOption = document.getElementById("mon-toggle").value;
+  if (selectedOption == 'yes' && element.classList.contains('not-showing')){
+    element.classList.remove("not-showing");
+    element.classList.add("showing");
+  }
+  else if (selectedOption == 'no' && element.classList.contains('showing')){
+    element.classList.remove("showing");
+    element.classList.add("not-showing");
   }
 }
+// function showTime(day, event) {
+//   event.preventDefault();
+//   var element = document.getElementById(day);
+//   if (element.style.display === "none") {
+//     element.style.display = "block"; // or "inline" depending on your needs
+//   } else {
+//     element.style.display = "none";
+//   }
+// }

d40afedc31d6e771979b29bc439a1a64da779f0a
JohnGiampaolo <giagio@LAPTOP-KA47AL5K>
2024-04-19 15:25:11 +0800

    Experimenting with monday slot. Trying to remove style based toggle and replacing with innerHTML approach. Issue: Will work but only after first instance.

--- pages/lobby-making.js
+++ pages/lobby-making.js
@@ -5,23 +5,24 @@ const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 
 days.forEach(function(day){
-  var elementID = day + "-toggle"
-  document.getElementById(elementID).addEventListener("change", showTime(day, elementID));
+  var toggleOption = day + "-toggle"
+  document.getElementById(toggleOption).addEventListener("change", showTime(day, toggleOption));
 });
 
 
-function showTime(day, elementID){
+function showTime(day, toggleOption){
   return function(){
     console.log("Change detected!");
     var element = document.getElementById(day);
-    var selectedOption = document.getElementById(elementID).value;
-    if (selectedOption == 'yes' && element.classList.contains('not-showing')){
-      element.classList.remove("not-showing");
-      element.classList.add("showing");
+    var selectedOption = document.getElementById(toggleOption).value;
+    if (selectedOption == 'yes' && element.innerHTML == ""){
+      element.innerHTML = "<label for='" + day + "-from'>From:</label>" +
+      "<input type='time' id='" + day + "-from' />" +
+      "<label for='" + day + "-to'> To: </label>" +
+      "<input type='time' id='" + day + "-to' />";
     }
-    else if (selectedOption == 'no' && element.classList.contains('showing')){
-      element.classList.remove("showing");
-      element.classList.add("not-showing");
+    else if (selectedOption == 'no' && element.innerHTML != ""){
+      element.innerHTML = "";
     }
   }
 }

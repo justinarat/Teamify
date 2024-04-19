f804297bfaf3cb4f11cbe392d6c33ef83c5a6850
JohnGiampaolo <giagio@LAPTOP-KA47AL5K>
2024-04-19 21:54:08 +0800

    Dropdown menu now correctly toggles general schedule on and off. When selecting calendar based, general schedule option disappears. By default dropdown is on Select which is becomes unselectable once changed.

--- pages/lobby-making.js
+++ pages/lobby-making.js
@@ -1,54 +1,73 @@
-console.log("Script is running");
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const daysFull = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
-const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]; 
-const daysFull = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-days.forEach(function(day){
-  var toggleOption = day + "-toggle"
-  document.getElementById(toggleOption).addEventListener("change", showGeneral(day, toggleOption));
-});
-
-function showGeneral(day, toggleOption){
-  return function(){
+function showGeneral(day, toggleOption) {
+  return function () {
     var dayElement = document.getElementById(day);
     let selectedOption = document.getElementById(toggleOption).value;
-    if (selectedOption == 'yes' && dayElement.innerHTML == ""){
-      dayElement.innerHTML = "<label for='" + day + "-from'>From:</label>" +
-      "<input type='time' id='" + day + "-from' />" +
-      "<label for='" + day + "-to'> To: </label>" +
-      "<input type='time' id='" + day + "-to' />";
-    }
-    else if (selectedOption == 'no' && dayElement.innerHTML != ""){
+    if (selectedOption == "yes" && dayElement.innerHTML == "") {
+      dayElement.innerHTML =
+        "<label for='" +
+        day +
+        "-from'>From:</label>" +
+        "<input type='time' id='" +
+        day +
+        "-from' />" +
+        "<label for='" +
+        day +
+        "-to'> To: </label>" +
+        "<input type='time' id='" +
+        day +
+        "-to' />";
+    } else if (selectedOption == "no" && dayElement.innerHTML != "") {
       dayElement.innerHTML = "";
     }
-  }
+  };
 }
 
 document.getElementById("time-type").addEventListener("change", timeDisplay);
 
-function timeDisplay(){
-  return function(){
-    console.log("Change detected");
-    var timeOptionChunk = document.getElementById("time-option");
-    let selectedOption = document.getElementById("time-type").value;
-    if (selectedOption == 'general' && timeOptionChunk == ""){
-      for (let i = 0; i < 7; i++){
-        let currDay = days[i];
-        let currDayFull = daysFull[i];
-        let htmlChunk = "<label for='" + currDay + "-toggle'>" + currDayFull + ":</label>" +
-        "<select id='" + currDay + "-toggle'>" +
+function timeDisplay() {
+  console.log("Change detected");
+  var timeOptionChunk = document.getElementById("time-option");
+  let selectedOption = document.getElementById("time-type").value;
+  if (selectedOption == "general" && timeOptionChunk.innerHTML == "") {
+    for (let i = 0; i < 7; i++) {
+      let currDay = days[i];
+      let currDayFull = daysFull[i];
+      let htmlChunk =
+        "<label for='" +
+        currDay +
+        "-toggle'>" +
+        currDayFull +
+        ":</label>" +
+        "<select id='" +
+        currDay +
+        "-toggle'>" +
         "  <option value='no'>No</option>" +
         "  <option value='yes'>Yes</option>" +
         "</select>" +
-        "<div id='" + currDay + "'></div>" +
+        "<div id='" +
+        currDay +
+        "'></div>" +
         "<br />";
-        timeOptionChunk.insertAdjacentHTML("afterend", htmlChunk);
-      }
+      timeOptionChunk.insertAdjacentHTML("beforeend", htmlChunk);
     }
-    else if (selectedOption != 'general' && timeOptionChunk != ""){
-      timeOptionChunk.innerHTML = "";
-    }
+    days.forEach(function (day) {
+      var toggleOption = day + "-toggle";
+      document
+        .getElementById(toggleOption)
+        .addEventListener("change", showGeneral(day, toggleOption));
+    });
+  } else if (selectedOption != "general" && timeOptionChunk.innerHTML != "") {
+    timeOptionChunk.innerHTML = "";
   }
 }
-
-

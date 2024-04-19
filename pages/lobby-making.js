e5535a7d86834650fefc38cb5b34677f261da389
JohnGiampaolo <giagio@LAPTOP-KA47AL5K>
2024-04-19 15:52:09 +0800

    Selecting Calendar Based mode in Time Allocation now removes the default General Based. However, need to implement a way to return General Based.

--- pages/lobby-making.js
+++ pages/lobby-making.js
@@ -2,27 +2,34 @@ console.log("Script is running");
 
 const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-
-
 days.forEach(function(day){
   var toggleOption = day + "-toggle"
-  document.getElementById(toggleOption).addEventListener("change", showTime(day, toggleOption));
+  document.getElementById(toggleOption).addEventListener("change", showGeneral(day, toggleOption));
 });
 
-
-function showTime(day, toggleOption){
+function showGeneral(day, toggleOption){
   return function(){
-    var element = document.getElementById(day);
-    var selectedOption = document.getElementById(toggleOption).value;
-    if (selectedOption == 'yes' && element.innerHTML == ""){
-      element.innerHTML = "<label for='" + day + "-from'>From:</label>" +
+    var dayElement = document.getElementById(day);
+    let selectedOption = document.getElementById(toggleOption).value;
+    if (selectedOption == 'yes' && dayElement.innerHTML == ""){
+      dayElement.innerHTML = "<label for='" + day + "-from'>From:</label>" +
       "<input type='time' id='" + day + "-from' />" +
       "<label for='" + day + "-to'> To: </label>" +
       "<input type='time' id='" + day + "-to' />";
     }
-    else if (selectedOption == 'no' && element.innerHTML != ""){
-      element.innerHTML = "";
+    else if (selectedOption == 'no' && dayElement.innerHTML != ""){
+      dayElement.innerHTML = "";
     }
+  }
+}
+
+document.getElementById("time-type").addEventListener("change", timeDisplay);
+
+function timeDisplay(){
+  var timeOptionChunk = document.getElementById("time-option");
+  let selectedOption = document.getElementById("time-type").value;
+  if (selectedOption != 'general' && timeOptionChunk != ""){
+    timeOptionChunk.innerHTML = "";
   }
 }
 

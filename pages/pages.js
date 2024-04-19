2805cf6da07a5cf1900b9b91f0f8029a6e3b8aba
JohnGiampaolo <giagio@LAPTOP-KA47AL5K>
2024-04-19 12:45:49 +0800

    added dropdown functionality in a more general sense

--- pages/pages.js
+++ pages/pages.js
@@ -1,25 +1,28 @@
 console.log("Script is running");
-document.getElementById("mon-toggle").addEventListener("change", showTime);
 
-function showTime(){
-  console.log("Change detected!");
-  var element = document.getElementById("monday");
-  var selectedOption = document.getElementById("mon-toggle").value;
-  if (selectedOption == 'yes' && element.classList.contains('not-showing')){
-    element.classList.remove("not-showing");
-    element.classList.add("showing");
-  }
-  else if (selectedOption == 'no' && element.classList.contains('showing')){
-    element.classList.remove("showing");
-    element.classList.add("not-showing");
+const days = ["mon", "tue"];
+
+
+
+days.forEach(function(day){
+  var elementID = day + "-toggle"
+  document.getElementById(elementID).addEventListener("change", showTime(day, elementID));
+});
+
+
+function showTime(day, elementID){
+  return function(){
+    console.log("Change detected!");
+    var element = document.getElementById(day);
+    var selectedOption = document.getElementById(elementID).value;
+    if (selectedOption == 'yes' && element.classList.contains('not-showing')){
+      element.classList.remove("not-showing");
+      element.classList.add("showing");
+    }
+    else if (selectedOption == 'no' && element.classList.contains('showing')){
+      element.classList.remove("showing");
+      element.classList.add("not-showing");
+    }
   }
 }
-// function showTime(day, event) {
-//   event.preventDefault();
-//   var element = document.getElementById(day);
-//   if (element.style.display === "none") {
-//     element.style.display = "block"; // or "inline" depending on your needs
-//   } else {
-//     element.style.display = "none";
-//   }
-// }
+

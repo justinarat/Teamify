344ac4630c08cd1d0d41b84824c5e8aad030bbb7
JohnGiampaolo <giagio@LAPTOP-KA47AL5K>
2024-04-26 14:17:25 +0800

    Removed hello world and test url's from routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,6 @@
 from app import app
 from flask import render_template
 
-# Both endpoints are temporary for testing
-@app.route("/")
-def hello():
-  return "Hello World!"
-
-@app.route("/test/route")
-def test():
-  return "Testing"
-
-# These endpoints could be in the final ver
 @app.route("/")
 @app.route("/introduction")
 def introduction():

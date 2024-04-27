8a6f6f00ddf43dbaea85dd0467880f410ef21fb7
FAljanobi <115057153+FAljanobi@users.noreply.github.com>
2024-04-28 02:42:12 +0800

    attempts at database port
    i think i ported the database but i cant tell its way too llate to tell

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
   # TODO: change this to actual db file, haven't done it yet as I'm not sure
   #       how it'll affect the db file
-  SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db") 
+  SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "Database","MainDB.db") 
   SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/model.py
+++ app/model.py
@@ -3,42 +3,57 @@ from app import db
 
 
 class Games(db.Model):
-  UID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-  Name = db.Column(db.Text(), nullable=False)
+    __tablename__ = 'Games' 
+    UID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    Name = db.Column(db.Text(), nullable=False)
 
-class Lobby(db.Model):
-  LobbyID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-  GameID = db.Column(db.Text(), nullable=False)
-  Desc = db.Column(db.Text(), nullable=False)
-  LobbyPlayersRel = db.relationship('LobbyPlayers', backref='Lobby', lazy=True)
-  LobbyTagsRel = db.relationship('LobbyTags', backref='Lobby', lazy=True)
-  LobbbyTimesRel = db.relationship('LobbbyTimes', backref='Lobby', lazy=True)
-
-class LobbyPlayers(db.Model):
-  RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-  LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID"), nullable=False)
-  UserID = db.Column(db.Text(), db.ForeignKey("Users.UID"), nullable=False)
-  Authority = db.Column(db.Text(), unique=True, nullable=False)
-
-class LobbyTags(db.Model):
-  RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-  LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID"), nullable=False)
-  TagID = db.Column(db.Text(), nullable=False)
-
-class LobbbyTimes(db.Model):
-  RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-  LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID"), nullable=False)
-  TimeBlockStart = db.Column(db.Text(), nullable=False)
-  Repeat = db.Column(db.Text(), unique=True, nullable=False)
-  TimeBlockEnd = db.Column(db.Text(), nullable=False)
 
 class Tags(db.Model):
-  TagID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-  Name = db.Column(db.Text(), nullable=False)
+    __tablename__ = 'Tags' 
+    TagID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    Name = db.Column(db.Text(), nullable=False)
 
 
 class Users(db.Model):
-  UID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-  Username = db.Column(db.Text(), nullable=False)
-  Password = db.Column(db.Text(), nullable=False)
-  Email = db.Column(db.Text(), unique=True, nullable=False)
+    __tablename__ = 'Users' 
+    UID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    Username = db.Column(db.Text(), nullable=False)
+    Password = db.Column(db.Text(), nullable=False)
+    Email = db.Column(db.Text(), unique=True, nullable=False)
+
+
+class Lobby(db.Model):
+    __tablename__ = 'Lobby' 
+    LobbyID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    GameID = db.Column(db.Text(), db.ForeignKey("Games.UID"), nullable=False)
+    Desc = db.Column(db.Text())
+    gamesRel = db.relationship('Games', backref='games', lazy=True)
+
+
+class LobbyPlayers(db.Model):
+    __tablename__ = 'LobbyPlayers' 
+    RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID"), nullable=False)
+    UserID = db.Column(db.Text(), db.ForeignKey("Users.UID"), nullable=False)
+    Authority = db.Column(db.Text(), nullable=False)
+    userRel = db.relationship('Users', backref='users', lazy=True)
+    lobbyRel = db.relationship('Lobby', backref='lobby3', lazy=True)
+
+
+class LobbyTags(db.Model):
+    __tablename__ = 'LobbyTags' 
+    RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID"), nullable=False)
+    TagID = db.Column(db.Text(), db.ForeignKey("Tags.TagID"), nullable=False)
+    tagRel = db.relationship('Tags', backref='tags', lazy=True)
+    lobbyRel = db.relationship('Lobby', backref='lobby2', lazy=True)
+
+
+class LobbyTimes(db.Model):
+    __tablename__ = 'LobbyTimes' 
+    RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID"), nullable=False)
+    TimeBlockStart = db.Column(db.Text(), nullable=False)
+    Repeat = db.Column(db.Text(), unique=True, nullable=False)
+    TimeBlockEnd = db.Column(db.Text(), nullable=False)
+    lobbyRel = db.relationship('Lobby', backref='lobby1', lazy=True)

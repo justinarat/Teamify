1bb4ef219d32d2ba9deccbf44e8ef42de4ad539d
Dominic Davies <dominictdavies@gmail.com>
2024-04-28 17:11:35 +0800

    Use GET requests instead of POST

--- tests/test_data_routes.py
+++ tests/test_data_routes.py
@@ -8,27 +8,28 @@ class DataRoutesTestCase(unittest.TestCase):
 
     def test_get_lobby_cards(self):
         """Tests the get_lobby_cards() endpoint in data_routes.py"""
-        self._test_cards_no_post_body()
+        self._test_cards_no_param()
         self._test_cards_count(1)
         self._test_cards_count(30)
         self._test_cards_data_format()
 
-    # TODO: Complete _test_cards_no_post_body implementation
-    def _test_cards_no_post_body(self):
+    # TODO: Complete implementation
+    def _test_cards_no_param(self):
         pass
 
-    def _test_cards_no_count(self):
-        """Tests if status 400 and empty response is returned if POST body is invalid (no count)"""
+    def _test_cards_incorrect_count(self):
+        """Tests if status 400 and empty response is returned if GET request is invalid (no count param)"""
         url = self.domain + "/get-lobby-cards"
-        response = requests.post(url)
+        params = {"count": ""}
+        response = requests.get(url, params=params)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.text, "")
 
     def _test_cards_count(self, count):
         """Tests if the number of lobby cards returned matches the number requested"""
         url = self.domain + "/get-lobby-cards"
-        count_json = {"count": count}
-        response = requests.post(url, json=count_json)
+        params = {"count": 1}
+        response = requests.get(url, params=params)
         self.assertEqual(response.status_code, 200)
         response_dict = response.json()
         self.assertEqual(len(response_dict["lobbies"]), count)
@@ -36,14 +37,16 @@ class DataRoutesTestCase(unittest.TestCase):
     def _test_cards_data_format(self):
         """Tests if the response data has the right lobby card keys and values"""
         url = self.domain + "/get-lobby-cards"
-        response = make_request(url, "POST", query_string="count=1")
-        lobby_card_dict = response.json()["lobby_cards"]
+        params = {"count": 1}
+        response = requests.get(url, params=params)
+        lobby_cards = response.json()["lobby_cards"]
+        self.assertEqual(response.status_code, 200)
         # TODO: Check if the format of the response was correct (has the correct keys and maybe values)
         #       Still need to decide on the format, but will likely have:
-        self.assertTrue("lobby_id" in lobby_card_dict)
-        self.assertTrue("game_title" in lobby_card_dict)
-        self.assertTrue("lobby_name" in lobby_card_dict)
-        self.assertTrue("lobby_description" in lobby_card_dict)
-        self.assertTrue("host" in lobby_card_dict)
-        self.assertTrue("players" in lobby_card_dict)
-        self.assertTrue("next_available_time" in lobby_card_dict)
+        self.assertTrue("lobby_id" in lobby_cards)
+        self.assertTrue("game_title" in lobby_cards)
+        self.assertTrue("lobby_name" in lobby_cards)
+        self.assertTrue("lobby_description" in lobby_cards)
+        self.assertTrue("host" in lobby_cards)
+        self.assertTrue("players" in lobby_cards)
+        self.assertTrue("next_available_time" in lobby_cards)

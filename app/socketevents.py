14b7ece440fa017d99725a6eba8ac43fcb68a896
Justin Arat <aratjustin@gmail.com>
2024-04-30 00:17:06 +0800

    Add docstrings to functions in socketevents.py

--- app/socketevents.py
+++ app/socketevents.py
@@ -17,6 +17,13 @@ import sys
 #   have been done when the player joins the server (in the lobby_view endpoint)
 @socketio.on("player_join")
 def player_join():
+    """Broadcasts the new player's username to players.
+    
+    Sent data is in the format:
+        data_to_send = {
+            "sender_username": "sender_username", 
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     join_room(lobby_id)
     sender_username = "USERNAME" # TODO: Query database for sender username
@@ -27,6 +34,13 @@ def player_join():
 
 @socketio.on("player_leave")
 def player_leave():
+    """Broadcasts the username of the player that left.
+    
+    Sent data is in the format:
+        data_to_send = {
+            "sender_username": "sender_username" 
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     # TODO: remove player from lobby in database
     session.pop("lobby_id")
@@ -48,6 +62,19 @@ def player_kick(data):
 # Events all users can send
 @socketio.on("player_text")
 def player_text(data):
+    """Broadcasts the player's username and text message.
+
+    data is in the format:
+        data = {
+            "body": "text_message"
+        }
+
+    Sent data is in the format:
+        data_to_send = {
+            "sender_username": "sender_username", 
+            "body": "text_message"
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     sender_username = "USERNAME" # TODO: Query database for sender username
     text_message = data["body"]
@@ -61,6 +88,18 @@ def player_text(data):
 # Events only the host (or lobby mods) can send
 @socketio.on("add_tag")
 def add_tag(data):
+    """Broadcasts the tag that was added.
+
+    data is in the format:
+        data = {
+            "body": "tag"
+        }
+
+    Sent data is in the format:
+        data_to_send = {
+            "body": "tag"
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     if not is_lobby_host(sender_user_id): return
     tag = data["body"]
@@ -71,6 +110,18 @@ def add_tag(data):
 
 @socketio.on("remove_tag")
 def remove_tag(data):
+    """Broadcasts the tag that was removed.
+
+    data is in the format:
+        data = {
+            "body": "tag"
+        }
+
+    Sent data is in the format:
+        data_to_send = {
+            "body": "tag"
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     if not is_lobby_host(sender_user_id): return
     tag = data["body"]
@@ -81,6 +132,18 @@ def remove_tag(data):
 
 @socketio.on("change_lobby_name")
 def change_lobby_name(data):
+    """Broadcasts the new lobby name.
+
+    data is in the format:
+        data = {
+            "body": "new lobby name"
+        }
+
+    Sent data is in the format:
+        data_to_send = {
+            "body": "new lobby name"
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     if not is_lobby_host(sender_user_id): return
     new_lobby_name = data["body"]
@@ -91,6 +154,18 @@ def change_lobby_name(data):
 
 @socketio.on("change_description")
 def change_description(data):
+    """Broadcasts the new description.
+
+    data is in the format:
+        data = {
+            "body": "new description"
+        }
+
+    Sent data is in the format:
+        data_to_send = {
+            "body": "new description"
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     if not is_lobby_host(sender_user_id): return
     new_description = data["body"]
@@ -101,6 +176,18 @@ def change_description(data):
 
 @socketio.on("change_time_schedule")
 def change_time_schedule(data):
+    """Broadcasts the new time schedule.
+
+    data is in the format:
+        data = {
+            "body": "new time schedule"
+        }
+
+    Sent data is in the format:
+        data_to_send = {
+            "body": "new time schedule"
+        }
+    """
     sender_user_id, lobby_id = session["user_id"], session["lobby_id"]
     if not is_lobby_host(sender_user_id): return
     new_time_schedule = data["body"]
@@ -110,5 +197,6 @@ def change_time_schedule(data):
     emit("change_time_schedule", data_to_send, to=lobby_id)
 
 def is_lobby_host(user_id):
+    """Checks if user_id is the host of lobby_id."""
     # TODO
     return True

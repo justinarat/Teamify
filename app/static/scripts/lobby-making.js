43104a651d080ad950283f72d53660cae730e3f2
JohnGiampaolo <giagio@LAPTOP-KA47AL5K>
2024-05-03 15:36:24 +0800

    Moved special event button to toggleable time schedule section

--- app/static/scripts/lobby-making.js
+++ app/static/scripts/lobby-making.js
@@ -33,6 +33,9 @@ function timeDisplay() {
         "</div>";
       timeOptionChunk.insertAdjacentHTML("beforeend", htmlChunk);
     }
+    let htmlChunk =
+      "<button id='add-special-event' onclick='makeSpecialEvent()' type='button'>+ Add Special Event</button>"
+    timeOptionChunk.insertAdjacentHTML("beforeend", htmlChunk);
   } else {
     timeOptionChunk.innerHTML = "";
   }
@@ -111,52 +114,3 @@ function enableSkills() {
     skillLevelField.innerHTML = "";
   }
 }
-
-// function timeDisplay() {
-//   var selectedOption = document.getElementById("time-type");
-//   var timeOptionChunk = document.getElementById("time-option");
-//   if (selectedOption.checked == true) {
-//     for (let i = 0; i < 7; i++) {
-//       let currDay = days[i];
-//       let currDayFull = daysFull[i];
-//       let htmlChunk =
-//         "<input type='checkbox' id='" + currDay + "-checkbox'>" +
-//         "<label for='" +currDay + "-checkbox'>" + currDayFull + ":</label>" +
-//         "<div id='" + currDay + "'></div>" +
-//         "<br />";
-//       timeOptionChunk.insertAdjacentHTML("beforeend", htmlChunk);
-//     }
-//     days.forEach(function (day) {
-//       var toggleOption = day + "-checkbox";
-//       document
-//         .getElementById(toggleOption)
-//         .addEventListener("change", showGeneral(day, toggleOption));
-//     });
-//   } else {
-//     timeOptionChunk.innerHTML = "";
-//   }
-// }
-
-// function showGeneral(this) {
-//   return function () {
-//     var dayElement = document.getElementById(day);
-//     let selectedOption = document.getElementById(toggleOption);
-//     if (selectedOption.checked == true) {
-//       dayElement.innerHTML =
-//         "<label for='" +
-//         day +
-//         "-from'>From:</label>" +
-//         "<input type='time' id='" +
-//         day +
-//         "-from' />" +
-//         "<label for='" +
-//         day +
-//         "-to'> To: </label>" +
-//         "<input type='time' id='" +
-//         day +
-//         "-to' />";
-//     } else {
-//       dayElement.innerHTML = "";
-//     }
-//   };
-// }

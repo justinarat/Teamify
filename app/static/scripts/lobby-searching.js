645bb2c2306aa99f599f5da69383fd357a878b4a
Dominic Davies <dominictdavies@gmail.com>
2024-05-06 17:56:56 +0800

    WIP attempts at rendering cards

--- app/static/scripts/lobby-searching.js
+++ app/static/scripts/lobby-searching.js
@@ -1,5 +1,4 @@
-function helloWorld() {
-  console.log("Hello world!");
-}
-
-helloWorld();
+$(document).ready(function () {
+  console.log("Page is now ready");
+  $("h1").load("templates/lobby-card.html");
+});

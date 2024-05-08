f4f6dba85a37d583fc7291046e8962cb806cbb74
Justin Arat <aratjustin@gmail.com>
2024-05-08 22:31:02 +0800

    Change tab space to 4 in forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,12 +3,12 @@ from wtforms import EmailField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SignUpForm(FlaskForm):
-  email = EmailField("Email", validators=[DataRequired()])
-  username = StringField("Username", validators=[DataRequired()])
-  password = PasswordField("Password", validators=[DataRequired()])
-  submit = SubmitField("Sign Up")
+    email = EmailField("Email", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
 
 class LoginForm(FlaskForm):
-  username = StringField("Username", validators=[DataRequired()])
-  password = PasswordField("Password", validators=[DataRequired()])
-  submit = SubmitField("Login")
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")

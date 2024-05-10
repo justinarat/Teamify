b9284d0050047517ec4715f355b7f2ac4c22a8ae
FAljanobi <115057153+FAljanobi@users.noreply.github.com>
2024-05-10 11:39:03 +0800

    Update routes.py
    added incomplete login code

--- app/routes.py
+++ app/routes.py
@@ -66,8 +66,28 @@ def signup_request():
   """Handles signup form requests"""
   signup_form = SignUpForm()
   if signup_form.validate_on_submit():
-    # TODO: Use Flask Login to handle signup data
-    #       Then update the database
+    username = signup_form.username.data
+    password = signup_form.password.data
+    email = signup_form.email.data
+    print(username, file=sys.stderr)
+    print(password, file=sys.stderr)
+    print(email, file=sys.stderr)
+    # Check if username and password match in the database
+    user = Users.query.filter_by(Email=email).first()
+    print(user, file=sys.stderr)
+
+    if email!=None:
+        # Authentication successful, redirect to some page
+        print('New email', file=sys.stderr)
+        login_user(user)
+        return redirect(url_for("games_view"))
+    else:
+        # Authentication failed, redirect back to login page
+        print('Invalid Email', file=sys.stderr)
+        flash("Invalid Email")
+        login_form = LoginForm({}) # {} is to init signup_form with empty data as for some reason it shares data with login_form
+        return render_template("account-creation.html", title="Login or Sign Up", 
+                login_form=login_form, signup_form=signup_form)
     return redirect(url_for("games_view"))
 
 @app.route("/admin")

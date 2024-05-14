b7517e86d4ff34f960addc1184f29ca2fc639d7c
Dominic Davies <dominictdavies@gmail.com>
2024-05-14 17:55:16 +0800

    Pass most tests

--- app/data_routes.py
+++ app/data_routes.py
@@ -2,7 +2,7 @@
 
 from flask import request, make_response, jsonify
 from app import app, db
-from app.model import Lobby
+from app.model import Lobby, LobbyPlayers, LobbyTags, LobbyTimes, Games
 
 
 @app.route("/get-lobby-cards", methods=["GET"])
@@ -49,17 +49,33 @@ def get_lobby_cards():
     except ValueError:
         return make_response("Parameter 'count' must be an integer", 400)
 
-    if count <= 0:
-        return make_response("Parameter 'count' must be greater than 0", 400)
+    if count < 0:
+        return make_response("Parameter 'count' must be greater than or equal to 0", 400)
 
     lobby_cards = []
 
     for i in range(count):
-        query = db.session.query(Lobby).filter_by(LobbyID=i+1).first()
-        lobby_cards.append(query)
+        lobby_id = i + 1
+        lobby_query = db.session.query(Lobby).filter_by(LobbyID=lobby_id).first()
+
+        if lobby_query:
+            game_query = (
+                db.session.query(Games).filter_by(UID=lobby_query.GameID).first()
+            )
+            players_query = db.session.query(LobbyPlayers).filter_by(LobbyID=lobby_id)
+            lobby_card = {
+                "lobby_id": lobby_query.LobbyID,
+                "game_title": game_query.Name,
+                "lobby_name": "Lobby Name",
+                "lobby_description": lobby_query.Desc,
+                "host": "Host Name",
+                "players": players_query.all(),
+                "next_available_time": "Time",
+            }
+            lobby_cards.append(lobby_card)
 
     # TODO:
     # search_tags = request.args.getlist("search_tags")
     # ignore_tags = request.args.getlist("ignore_tags")
 
-    return jsonify({ "lobby_cards": lobby_cards })
+    return jsonify({"lobby_cards": lobby_cards})

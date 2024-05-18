fb3afcaa2914393aa7aef61eff2062d345f54e6d
justinarat <justinarat@yahoo.com.au>
2024-05-18 18:10:04 +0800

    Fix models to match with db
    Remove tag groups and suggestions
    Rename LobbyId to LobbyID

--- app/model.py
+++ app/model.py
@@ -15,8 +15,8 @@ class Tags(db.Model):
     __tablename__ = 'Tags'
     TagID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
     Name = db.Column(db.Text(), nullable=False)
-    TagGroup = db.Column(db.Text())
-    Suggestion = db.Column(db.Integer(), default=0)
+    #TagGroup = db.Column(db.Text())
+    #Suggestion = db.Column(db.Integer(), default=0)
 
 
 class Users(UserMixin, db.Model):
@@ -43,7 +43,7 @@ def load_student(user_id):
 
 class Lobby(db.Model):
     __tablename__ = 'Lobby'
-    LobbyId = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
+    LobbyID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
     GameID = db.Column(db.Text(), db.ForeignKey("Games.UID", name="fk_lobby_game"), nullable=False)
     Desc = db.Column(db.Text())
     game = db.relationship('Games', backref='games', lazy=True)
@@ -71,7 +71,7 @@ class Lobby(db.Model):
 class LobbyPlayers(db.Model):
     __tablename__ = 'LobbyPlayers'
     RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-    LobbyId = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyId", name="fk_lobby_players_lobby"), nullable=False)
+    LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID", name="fk_lobby_players_lobby"), nullable=False)
     UserID = db.Column(db.Text(), db.ForeignKey("Users.UID", name="fk_lobby_players_user"), nullable=False)
     Authority = db.Column(db.Text(), nullable=False)
 
@@ -82,16 +82,14 @@ class LobbyPlayers(db.Model):
 class LobbyTags(db.Model):
     __tablename__ = 'LobbyTags'
     RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-    LobbyId = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyId", name="fk_lobby_tags_lobby"), nullable=False)
+    LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID", name="fk_lobby_tags_lobby"), nullable=False)
     TagID = db.Column(db.Text(), db.ForeignKey("Tags.TagID", name="fk_lobby_tags_tag"), nullable=False)
-    tagRel = db.relationship('Tags', backref='tags', lazy=True)
-    lobbyRel = db.relationship('Lobby', backref='lobby2', lazy=True)
 
 
 class LobbyTimes(db.Model):
     __tablename__ = 'LobbyTimes'
     RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-    LobbyId = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyId", name="fk_lobby_times_lobby"), nullable=False)
+    LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID", name="fk_lobby_times_lobby"), nullable=False)
     TimeBlockStart = db.Column(db.Text(), nullable=False)
     Repeat = db.Column(db.Text(), unique=True, nullable=False)
     TimeBlockEnd = db.Column(db.Text(), nullable=False)
@@ -101,7 +99,7 @@ class LobbyTimes(db.Model):
 class UserTracker(db.Model):
     __tablename__ = 'UserTracker'
     RowID = db.Column(db.Text(), primary_key=True, unique=True, nullable=False)
-    LobbyId = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyId", name="fk_user_tracker_lobby"), nullable=False)
+    LobbyID = db.Column(db.Text(), db.ForeignKey("Lobby.LobbyID", name="fk_user_tracker_lobby"), nullable=False)
     UserID = db.Column(db.Text(), db.ForeignKey("Users.UID", name="fk_user_tracker_user"), nullable=False)
     Action = db.Column(db.Text(), nullable=False)
     Desc = db.Column(db.Text())

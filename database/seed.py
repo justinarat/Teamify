113479fb088f11a39d9e8ea573fe277ade91b1e5
Dominic Davies <dominictdavies@gmail.com>
2024-05-20 01:50:12 +0800

    Printing seed progress

--- database/seed.py
+++ database/seed.py
@@ -8,15 +8,16 @@ from app.model import (
     Games,
     Tags,
     Users,
+    UserTracker,
     Lobby,
     LobbyPlayers,
     LobbyTags,
     LobbyTimes,
-    UserTracker,
 )
 
 
 def empty_db():
+    print("Emptying database...")
     db.session.query(Games).delete()
     db.session.query(Tags).delete()
     db.session.query(Users).delete()
@@ -25,29 +26,28 @@ def empty_db():
     db.session.query(LobbyPlayers).delete()
     db.session.query(LobbyTags).delete()
     db.session.query(LobbyTimes).delete()
-    print("Database emptied successfully!")
 
 
 def seed_games():
-    games = [
+    data = [
         Games(UID="1", Name="Game1"),
         Games(UID="2", Name="Game2"),
     ]
 
-    db.session.add_all(games)
+    seed_table("Games", data)
 
 
 def seed_tags():
-    tags = [
+    data = [
         Tags(TagID="1", Name="Tag1"),
         Tags(TagID="2", Name="Tag2"),
     ]
 
-    db.session.add_all(tags)
+    seed_table("Tags", data)
 
 
 def seed_users():
-    users = [
+    data = [
         Users(
             UID="1", Username="User1", Password="password1", Email="user1@example.com"
         ),
@@ -56,11 +56,11 @@ def seed_users():
         ),
     ]
 
-    db.session.add_all(users)
+    seed_table("Users", data)
 
 
 def seed_user_tracker():
-    user_tracker = [
+    data = [
         UserTracker(
             RowID="1",
             LobbyID="1",
@@ -77,29 +77,29 @@ def seed_user_tracker():
         ),
     ]
 
-    db.session.add_all(user_tracker)
+    seed_table("UserTracker", data)
 
 
 def seed_lobbies():
-    lobbies = [
+    data = [
         Lobby(LobbyID="1", GameID="1", Desc="Lobby1", Name="First Lobby"),
         Lobby(LobbyID="2", GameID="2", Desc="Lobby2", Name="Second Lobby"),
     ]
 
-    db.session.add_all(lobbies)
+    seed_table("Lobby", data)
 
 
 def seed_lobby_players():
-    lobby_players = [
+    data = [
         LobbyPlayers(RowID="1", LobbyID="1", UserID="1", IsHost=1),
         LobbyPlayers(RowID="2", LobbyID="2", UserID="2", IsHost=0),
     ]
 
-    db.session.add_all(lobby_players)
+    seed_table("LobbyPlayers", data)
 
 
 def seed_lobby_times():
-    lobby_times = [
+    data = [
         LobbyTimes(
             RowID="1",
             LobbyID="1",
@@ -116,14 +116,19 @@ def seed_lobby_times():
         ),
     ]
 
-    db.session.add_all(lobby_times)
+    seed_table("LobbyTimes", data)
+
+
+def seed_table(table, data):
+    print(f"Seeding {table}...")
+    db.session.add_all(data)
 
 
 def seed_required():
     seed_games()
     # TODO: Seed Admin
 
-    print("Database seeded with required data!")
+    print("Database committed with required seed data!")
 
 
 def seed_all():
@@ -143,7 +148,7 @@ def seed_all():
     # Commit all changes
     db.session.commit()
 
-    print("Database seeded with all data!")
+    print("Database committed with all seed data!")
 
 
 if __name__ == "__main__":

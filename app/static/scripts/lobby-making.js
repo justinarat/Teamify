e946a03b7c54cb62fe4ee4551ffe830188801fd6
JohnGiampaolo <johnnyblocks03@gmail.com>
2024-05-19 11:52:17 +0800

    Implemented dynamic required validators for time fields if user decides to select a day

--- app/static/scripts/lobby-making.js
+++ app/static/scripts/lobby-making.js
@@ -14,9 +14,20 @@ function showGeneral(day) {
   let dayCheckboxID = day + "-checkbox";
   let dayCheckbox = document.getElementById(dayCheckboxID);
 
+  let fromFieldID = day + "-from";
+  let toFieldID = day + "-to";
+
+  let fromField = document.getElementById(fromFieldID);
+  let toField = document.getElementById(toFieldID);
+
   if(dayCheckbox.checked == true) {
     daySec.classList.remove("not-showing");
+    fromField.setAttribute('required','required');
+    toField.setAttribute('required','required');
+
   } else {
     daySec.classList.add("not-showing");
+    fromField.removeAttribute('required','required');
+    toField.removeAttribute('required','required');
   }
 }

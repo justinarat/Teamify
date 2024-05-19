b77ebf3e530d8a34b2e4057800e2a8910a8cdacc
JohnGiampaolo <johnnyblocks03@gmail.com>
2024-05-19 11:39:20 +0800

    Removed javascript functions that are no longer necessary

--- app/static/scripts/lobby-making.js
+++ app/static/scripts/lobby-making.js
@@ -10,13 +10,6 @@ const daysFull = [
 ];
 
 function showGeneral(day) {
-  // let sectionID = dayCheckbox.id;
-  // let day = sectionID.substring(0, 3);
-  // // let dayElement = document.getElementById("day");
-  // let dayMainID = dayCheckbox.parentNode.id;
-  // let dayMain = document.getElementById(dayMainID);
-  // let dayElement = dayMain.children[2];
-  
   let daySec = document.getElementById(day);
   let dayCheckboxID = day + "-checkbox";
   let dayCheckbox = document.getElementById(dayCheckboxID);
@@ -26,23 +19,4 @@ function showGeneral(day) {
   } else {
     daySec.classList.add("not-showing");
   }
-}
-
-let customCount = 0;
-let addCustomTagButton = document.getElementById("add-custom-tag");
-
-addCustomTagButton.addEventListener("click", function () {
-  customCount++;
-});
-
-function showCustomTag() {
-  let tagSec = document.getElementById("tag1");
-  tagSec.classList.remove("not-showing");
-  tagSec.value = "true";
-}
-
-function removeCustom(tag) {
-  let tagSec = document.getElementById(tag);
-  tagSec.classList.add("not-showing");
-  tagSec.value = "false";
 }

6dcda6742c43fb3058546f99531b6d41b2cabfbf
Dominic Davies <dominictdavies@gmail.com>
2024-05-19 15:16:20 +0800

    More formatting

--- app/static/scripts/lobby-searching.js
+++ app/static/scripts/lobby-searching.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
   loadLobbyCards(DEFAULT_LOBBY_CARD_COUNT, ""); // "world" is temp
 
-  $("#lobby-search").submit(function(e) {
+  $("#lobby-search").submit(function (e) {
     e.preventDefault();
     clearLobbyCards();
 
@@ -35,22 +35,25 @@ function parseSearch(search) {
   }
 }
 
-function loadLobbyCards(count, searchString, searchTags=[]) {
-  let url =  "/get-lobby-cards?count=" + count;
+function loadLobbyCards(count, searchString, searchTags = []) {
+  let url = "/get-lobby-cards?count=" + count;
   url += "&search_string=" + searchString;
   for (tag in searchTags) {
     url += "&search_tags=" + tag;
   }
 
-  $.ajax({url: url, success: function(lobbyCards) {
-    for (let lobbyCardData of lobbyCards.lobby_cards) {
-      const lobbyCard = createLobbyCard(lobbyCardData);
-      $("#lobbies").append(lobbyCard);
-    }
-  }});
+  $.ajax({
+    url: url,
+    success: function (lobbyCards) {
+      for (let lobbyCardData of lobbyCards.lobby_cards) {
+        const lobbyCard = createLobbyCard(lobbyCardData);
+        $("#lobbies").append(lobbyCard);
+      }
+    },
+  });
 }
 
-/** Creates a lobby card html element 
+/** Creates a lobby card html element
  *
  *  @param lobbyCardData Lobby card data received from /get-lobby-card
  *
@@ -75,7 +78,8 @@ function createLobbyCard(lobbyCardData) {
 
   const lobbyDescriptionParagraph = document.createElement("p");
   lobbyDescriptionParagraph.classList.add("lobby-description");
-  lobbyDescriptionParagraph.innerHTML = "desc = " + lobbyCardData.lobby_description;
+  lobbyDescriptionParagraph.innerHTML =
+    "desc = " + lobbyCardData.lobby_description;
 
   const lobbyHostDiv = document.createElement("div");
   lobbyHostDiv.classList.add("lobby-host");
@@ -83,7 +87,7 @@ function createLobbyCard(lobbyCardData) {
 
   const otherLobbyPlayersDiv = document.createElement("div");
   otherLobbyPlayersDiv.classList.add("lobby-other-players");
-  otherLobbyPlayersDiv.innerHTML = "players = "
+  otherLobbyPlayersDiv.innerHTML = "players = ";
   for (let i = 0; i < lobbyCardData.players.length; i++) {
     otherLobbyPlayersDiv.innerHTML += lobbyCardData.players[i] + " ";
   }
@@ -101,13 +105,12 @@ function createLobbyCard(lobbyCardData) {
   lobbyCardBodyDiv.appendChild(otherLobbyPlayersDiv);
   lobbyCardBodyDiv.appendChild(timeTableDiv);
 
-  gameNameHeader.addEventListener("click", function(e) {
+  gameNameHeader.addEventListener("click", function (e) {
     e.preventDefault();
     lobbyID = lobbyCardData.lobby_id;
     window.location.pathname.replace("lobby");
     window.location.search.replace("lobby_id=" + lobbyID);
   });
 
-  return lobbyCard
+  return lobbyCard;
 }
-

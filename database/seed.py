32827594d391d4f9bb94b3f0f3fe7230521eea63
Dominic Davies <dominictdavies@gmail.com>
2024-05-19 23:56:59 +0800

    Split into many functions

--- database/seed.py
+++ database/seed.py
@@ -16,17 +16,37 @@ from app.model import (
 )
 
 
-def seed_data():
+def empty_db():
+    db.session.query(Games).delete()
+    db.session.query(Tags).delete()
+    db.session.query(Users).delete()
+    db.session.query(UserTracker).delete()
+    db.session.query(Lobby).delete()
+    db.session.query(LobbyPlayers).delete()
+    db.session.query(LobbyTags).delete()
+    db.session.query(LobbyTimes).delete()
+    print("Database emptied successfully!")
+
+
+def seed_games():
     games = [
         Games(UID="1", Name="Game1"),
         Games(UID="2", Name="Game2"),
     ]
 
+    db.session.add_all(games)
+
+
+def seed_tags():
     tags = [
         Tags(TagID="1", Name="Tag1"),
         Tags(TagID="2", Name="Tag2"),
     ]
 
+    db.session.add_all(tags)
+
+
+def seed_users():
     users = [
         Users(
             UID="1", Username="User1", Password="password1", Email="user1@example.com"
@@ -36,38 +56,10 @@ def seed_data():
         ),
     ]
 
-    lobbies = [
-        Lobby(LobbyID="1", GameID="1", Desc="Lobby1", Name="First Lobby"),
-        Lobby(LobbyID="2", GameID="2", Desc="Lobby2", Name="Second Lobby"),
-    ]
+    db.session.add_all(users)
 
-    lobby_players = [
-        LobbyPlayers(RowID="1", LobbyID="1", UserID="1", IsHost=1),
-        LobbyPlayers(RowID="2", LobbyID="2", UserID="2", IsHost=0),
-    ]
 
-    lobby_tags = [
-        LobbyTags(RowID="1", LobbyID="1", TagID="1"),
-        LobbyTags(RowID="2", LobbyID="2", TagID="2"),
-    ]
-
-    lobby_times = [
-        LobbyTimes(
-            RowID="1",
-            LobbyID="1",
-            TimeBlockStart="08:00",
-            DayOfWeek="0",
-            TimeBlockEnd="10:00",
-        ),
-        LobbyTimes(
-            RowID="2",
-            LobbyID="2",
-            TimeBlockStart="09:00",
-            DayOfWeek="1",
-            TimeBlockEnd="11:00",
-        ),
-    ]
-
+def seed_user_tracker():
     user_tracker = [
         UserTracker(
             RowID="1",
@@ -85,21 +77,75 @@ def seed_data():
         ),
     ]
 
-    # Add the data to the session and commit
-    db.session.add_all(games)
-    db.session.add_all(tags)
-    db.session.add_all(users)
-    db.session.add_all(lobbies)
-    db.session.add_all(lobby_players)
-    db.session.add_all(lobby_tags)
-    db.session.add_all(lobby_times)
     db.session.add_all(user_tracker)
 
+
+def seed_lobbies():
+    lobbies = [
+        Lobby(LobbyID="1", GameID="1", Desc="Lobby1", Name="First Lobby"),
+        Lobby(LobbyID="2", GameID="2", Desc="Lobby2", Name="Second Lobby"),
+    ]
+
+    db.session.add_all(lobbies)
+
+
+def seed_lobby_players():
+    lobby_players = [
+        LobbyPlayers(RowID="1", LobbyID="1", UserID="1", IsHost=1),
+        LobbyPlayers(RowID="2", LobbyID="2", UserID="2", IsHost=0),
+    ]
+
+    db.session.add_all(lobby_players)
+
+
+def seed_lobby_times():
+    lobby_times = [
+        LobbyTimes(
+            RowID="1",
+            LobbyID="1",
+            TimeBlockStart="08:00",
+            DayOfWeek="0",
+            TimeBlockEnd="10:00",
+        ),
+        LobbyTimes(
+            RowID="2",
+            LobbyID="2",
+            TimeBlockStart="09:00",
+            DayOfWeek="1",
+            TimeBlockEnd="11:00",
+        ),
+    ]
+
+    db.session.add_all(lobby_times)
+
+
+def seed_required():
+    seed_games()
+    # TODO: Seed Admin
+
+    print("Database seeded with required data!")
+
+
+def seed_all():
+    # Empty the database
+    empty_db()
+
+    # Seed all data
+    seed_games()
+    seed_tags()
+    seed_users()
+    seed_lobbies()
+    seed_lobby_players()
+    # TODO: Seed lobby_tags
+    seed_lobby_times()
+    seed_user_tracker()
+
+    # Commit all changes
     db.session.commit()
 
-    print("Database seeded successfully!")
+    print("Database seeded with all data!")
 
 
 if __name__ == "__main__":
     with app.app_context():
-        seed_data()
+        seed_all()
